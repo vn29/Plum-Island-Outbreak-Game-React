36b0006bb16ba5dc28f33a2e96e7c4b70d0c8305
vn29 <41770458+vn29@users.noreply.github.com>
2019-10-18 23:02:20 -0700

    Updates

---
+++ precache-manifest.278696168e9a6719de6cdad61253eda5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578722bfa78748dbf54d4577b104bff3",
+    "url": "/Plum-Island-Outbreak-Game-React/index.html"
+  },
+  {
+    "revision": "73e0cff3021697de59f7",
+    "url": "/Plum-Island-Outbreak-Game-React/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0c141c1076c1a31bd459",
+    "url": "/Plum-Island-Outbreak-Game-React/static/css/main.d1801c02.chunk.css"
+  },
+  {
+    "revision": "73e0cff3021697de59f7",
+    "url": "/Plum-Island-Outbreak-Game-React/static/js/2.bc05e829.chunk.js"
+  },
+  {
+    "revision": "0c141c1076c1a31bd459",
+    "url": "/Plum-Island-Outbreak-Game-React/static/js/main.aa2a9ca5.chunk.js"
+  },
+  {
+    "revision": "bf7b2f2d48514d2e2f9b",
+    "url": "/Plum-Island-Outbreak-Game-React/static/js/runtime-main.d438fad8.js"
+  },
+  {
+    "revision": "777a7ce20dfc89bcef292ac2807684db",
+    "url": "/Plum-Island-Outbreak-Game-React/static/media/oSk3FOL-biohazard-wallpaper.777a7ce2.jpg"
+  }
+]);
